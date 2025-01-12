912d70af50aec2c121ffece5b0bf9d1a55b267a5
Mariano <97608505+Ferbinand@users.noreply.github.com>
2025-01-12 03:05:45 -0300

    movimiento y personajes

---
+++ scene/personaje.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+# Velocidad y fuerza de salto
+@export var speed := 200
+@export var jump_force := -400
+@export var gravity := 800
+
+func _physics_process(delta):
+	# Movimiento lateral
+	if Input.is_action_pressed("mover_izquierda"):
+		velocity.x = -speed
+	elif Input.is_action_pressed("mover_derecha"):
+		velocity.x = speed
+	else:
+		velocity.x = 0
+
+	# Salto
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = jump_force
+		tomar_daño(20)
+
+	# Aplicar gravedad
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+
+
+	# Mover el personaje
+	move_and_slide()
+
+var vida := 100
+# Función para que el personaje reciba daño
+func tomar_daño(cantidad):
+	vida -= cantidad
+	if vida <= 0:
+		vida = 0
+		print("El personaje ha muerto")  # Esto puede reemplazarse con una lógica de muerte, como reiniciar el nivel.
