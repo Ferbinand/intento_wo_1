f78b171f57329f152d8aed4a2bf96fef2a832738
Mariano <97608505+Ferbinand@users.noreply.github.com>
2025-01-15 01:43:30 -0300

    item container para inventario

--- scripts/inventario.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-#hacer una signal para ejecutar el input event de Main en caso de que no funcione de esta manera. 
-#Ya que solo funciona si hacemos click en la pantalla principal
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_Y and $Window.visible:
-			print("Se cerró el inventario")
-			$Window.visible = false
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,18 +2,22 @@ extends Node2D
 
 var tiempo_partida = 100
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_E and !$Inventario/Window.visible:
-			$Inventario/Window.visible = true
-			print("Se abrio el inventario")
-		elif event.keycode == KEY_E and $Inventario/Window.visible:
-			$Inventario/Window.visible = false
-			print("Se cerró el inventario")
+
 
 func _ready() -> void:
 	print("partida empezada")
 	_iniciar_partida()
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_E and !$UI/Inventario.visible:
+			$UI/Inventario.visible = true
+			print("Se abrio el inventario")
+		elif event.keycode == KEY_E and $UI/Inventario.visible:
+			$UI/Inventario.visible = false
+			print("Se cerró el inventario")
+
+
 
 func actualizar_txt_timer():
 	$UI/LabelPartidaTimer.text = str(tiempo_partida)
